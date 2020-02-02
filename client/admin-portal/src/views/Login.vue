1a308a587a86c0c268a2c2a422f43f53a8447d53
JoeC <josephcrowley85@gmail.com>
2020-02-03 08:14:34 +1100

    created login view for admin

--- client/admin-portal/src/views/Login.vue
+++ client/admin-portal/src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-center">
+  <div class="text-center form-signin">
     <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
     <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
     <label for="inputEmail" class="sr-only">Email address</label>
@@ -7,10 +7,11 @@
     <label for="inputPassword" class="sr-only">Password</label>
     <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
     <div class="checkbox mb-3">
+      <input class="mr-2" type="checkbox" id="remember-me">
       <label for="remember-me">
         Remember me
       </label>
-      <input type="checkbox" id="remember-me">
+      
     </div>
     <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
     <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
@@ -22,8 +23,41 @@ export default {
   name: "login",
   data() {
     return {
-      
+      email: "",
+      password: ""
     }
   }
 }
 </script>
+
+<style scoped>
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: 0 auto;
+}
+.form-signin .checkbox {
+  font-weight: 400;
+}
+.form-signin .form-control {
+  position: relative;
+  box-sizing: border-box;
+  height: auto;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
