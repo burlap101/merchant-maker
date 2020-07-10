38376c81a82279058cbaf26b18b419306b668b44
Joe <josephcrowley85@gmail.com>
2020-07-10 13:58:52 +1000

    made the admin portal display ygfa again

--- client/admin-portal/src/components/Navbar.vue
+++ client/admin-portal/src/components/Navbar.vue
@@ -1,6 +1,8 @@
 <template>
   <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Yamba Grafton First Aid</a>
+    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#"
+      >Yamba Grafton First Aid</a
+    >
     <!-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> -->
     <div class="navbar p-0">
       <ul class="navbar-nav">
